a82168d29599319f3b35b1a45cc4da54bf2e7964
MidhunaMathew <71742999+MidhunaMathew@users.noreply.github.com>
2021-06-04 13:18:24 +0530

    Create day20b.c

---
+++ day20b.c
@@ -0,0 +1,25 @@
+/*Write a C program to find the number of words in a string. Also reverse string.
+
+Input Format
+
+Input a String.
+
+Constraints
+
+Use the concept of functions in C for counting the words and reversing.
+
+Don't use built in functions.
+
+Output Format
+
+First line refers to number of words in the string.
+
+Second line refers to string after reversing.
+
+Sample Input 0
+
+hello world
+Sample Output 0
+
+no of words = 2
+dlrow olleh */
