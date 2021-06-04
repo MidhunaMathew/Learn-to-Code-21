4aa60b96eb5b1c5efc5dedaa0039b2faebc3de07
MidhunaMathew <71742999+MidhunaMathew@users.noreply.github.com>
2021-06-04 21:23:11 +0530

    Update day20b.c

--- day20b.c
+++ day20b.c
@@ -23,3 +23,33 @@ Sample Output 0
 
 no of words = 2
 dlrow olleh */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int reverse(char *str)
+{
+    int i,c =0,l;
+    for (i = 0;str[i]!='\0';i++)
+    {
+        if (str[i]==' ' && str[i+1]!=' ')
+            c++;    
+    }
+    printf("no of words = %d\n",c+1);
+    l=strlen(str);
+    for(i=l-1;i>=0;i--)
+    {
+        printf("%c",str[i]);
+    }
+    return 0;
+    
+}
+int main() 
+{  
+    char str[100];
+    gets(str);
+    reverse (str);
+    return 0;
+}
