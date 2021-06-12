e4befbc8eea0780dfeabfca3fa2c128ddd5fde3e
MidhunaMathew <71742999+MidhunaMathew@users.noreply.github.com>
2021-06-12 13:28:45 +0530

    Update MiniProject.c

--- MiniProject.c
+++ MiniProject.c
@@ -1,14 +1,1 @@
 /* */
-#include <stdio.h>
-int main(int reading)
-int main()
-{
-    int p1,p2,new;
-    printf("Enter the previous reading");
-    scanf("%d",&p1);
-    printf("Enter the present reading");
-    scanf("%d",&p2);
-    new=p2-p1;
-    reading(new);
-    return 0;
-}
