7f28214fa63bbce1146e7058a2dbbaa16b3d9423
MidhunaMathew <71742999+MidhunaMathew@users.noreply.github.com>
2021-06-13 08:40:40 +0530

    Update MiniProject.c

--- MiniProject.c
+++ MiniProject.c
@@ -1,14 +1,12 @@
-/*Calculate Electricity  bill*/
+/*C program to calculate Electricity  bill*/
 #include <stdio.h>
-int main()
+int reading(int units)
 {
-    int units,fixed=160;
+    int fixed=160;
     float meterrent=14.28,duty=256.50,total,bill;
-    printf("Enter the units:");
-    scanf("%d",&units);
-    if(units <= 100)
+    if(units <= 100) 
     {
-       bill = units*3.70;
+       bill = units*3.70; 
     }
     else if(units<=200)
     {
@@ -16,13 +14,29 @@ int main()
     }
     else if(units<=300)
     {
-        bill = 100*3.70 + 100*4.80 +(units-200)*6.40;
+        bill = 100*3.70 + 100*4.80 +(units-200)*5.40;
     }
     else if(units > 300)
     {
-        bill = 100*3.70 + 100*4.80 + 100*6.40 + (units-300)*7.60;
+        bill = 100*3.70 + 100*4.80 + 100*5.40 + (units-300)*6.10;
     }
-    total=bill+meterrent+duty+fixed;
+    total=bill+meterrent+duty+fixed; //total amount including taxes
     printf("Total amount = Rs%.2f",total);
     return 0;
 }
+int main()
+{
+    int units;
+    printf("Enter the units:"); //current meter reading - previous meter reading
+    scanf("%d",&units);
+    reading(units);
+    return 0;
+}
+
+
+/*OUTPUT
+Enter the units:340
+Total amount = Rs2064.78 */
+
+
+
